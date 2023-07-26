da8f750a8f18f55f662577bd387e091cf2ab1805
e-oz <normandiggs@gmail.com>
2023-07-26 08:57:04 +0200

    "...naming variables and +/-1"

--- projects/state-mgmt-signals/src/base/page-indicator/page-indicator.component.ts
+++ projects/state-mgmt-signals/src/base/page-indicator/page-indicator.component.ts
@@ -3,7 +3,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 @Component({
   selector: 'page-indicator',
   standalone: true,
-  template: `Page: {{page}}`,
+  template: `Page: {{page+1}}`,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PageIndicatorComponent {
